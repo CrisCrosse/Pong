52b478200e753c4ea073d855adce7aa296222385
chrisrossell <chrisrossell1999@gmail.com>
2026-05-09 10:05:55 +0100

    Initial attempt at playing with the JFrames and JComponents

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,46 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Welcome to Chris Rs Pong!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Create and set up the window.
+        JFrame frame = new JFrame("Pong");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel pongLabel = new JLabel("Pong label?");
+        Box box = new Box(BoxLayout.X_AXIS);
+        box.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.WHITE, Color.WHITE));
+        box.setBackground(Color.GREEN);
+        box.setOpaque(true);
+
+
+        pongLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(pongLabel, BorderLayout.CENTER);
+        box.setPreferredSize(new Dimension(600, 400));
+        frame.getContentPane().add(box, BorderLayout.CENTER);
+//        frame.setLocationRelativeTo(pongLabel);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+
+        try {
+            Graphics boxGraphics = box.getGraphics();
+            boxGraphics.setColor(Color.BLACK);
+            box.paintComponents(boxGraphics);
+            boxGraphics.setClip(100, 100, 600, 400);
+            boxGraphics.fillRect(100, 100, 600, 400);
+            box.paintComponents(boxGraphics);
+        } catch (Exception e) {
+            System.out.print(e);
         }
-    }
+
+        }
 }

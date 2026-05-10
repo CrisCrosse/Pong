b75775a20d6f6bfcefd74302c726f55f16695cff
chrisrossell <chrisrossell1999@gmail.com>
2026-05-10 14:30:49 +0100

    Add paddles and player paddle movement with arrow keys

--- src/main/java/org/example/MoveAction.java
+++ src/main/java/org/example/MoveAction.java
@@ -5,41 +5,68 @@ import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeListener;
 
 public class MoveAction implements Action {
+    private static final int MOVE_SPEED = 4;
+
+    private final Pong pong;
+    private final boolean movesUp;
+
+    MoveAction(Pong pong, boolean movesUp) {
+        this.pong = pong;
+        this.movesUp = movesUp;
+    }
+
     @Override
     public Object getValue(String key) {
-        System.out.print("action performed?");
+        System.out.println("getValue");
         return null;
     }
 
     @Override
     public void putValue(String key, Object value) {
-        System.out.print("action performed?");
+        System.out.println("putValue");
     }
 
     @Override
     public void setEnabled(boolean b) {
-        System.out.print("action performed?");
+        System.out.println("ZsetEnabled");
     }
 
     @Override
     public boolean isEnabled() {
-        System.out.print("action performed?");
-        return false;
+        System.out.println("checking if action is enabled");
+        return true;
     }
 
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-        System.out.print("action performed?");
+        System.out.println("addPropertChangeListener");
     }
 
     @Override
     public void removePropertyChangeListener(PropertyChangeListener listener) {
-        System.out.print("action performed?");
+        System.out.println("removePropertyChangeListener");
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.print("action performed?");
+        int currentY = pong.getPlayerPaddleY();
+        int minYPaddleValue = Pong.getTABLE_Y();
+        int maxYPaddleValue = Pong.getTABLE_Y() + Pong.getTABLE_HEIGHT() - Pong.getPADDLE_HEIGHT();
+
+        if (movesUp && currentY > minYPaddleValue) {
+            if (currentY - MOVE_SPEED < minYPaddleValue) {
+                pong.setPlayerPaddleY(minYPaddleValue);
+            } else {
+                pong.setPlayerPaddleY(currentY - MOVE_SPEED);
+            }
+        } else if (!movesUp && currentY < maxYPaddleValue) {
+            if (currentY + MOVE_SPEED > maxYPaddleValue) {
+                pong.setPlayerPaddleY(maxYPaddleValue);
+            } else {
+                pong.setPlayerPaddleY(currentY + MOVE_SPEED);
+            }
+        }
+        pong.repaint();
     }
 }
--- src/main/java/org/example/Pong.java
+++ src/main/java/org/example/Pong.java
@@ -1,25 +1,37 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Pong extends JPanel {
     private static final int TABLE_WIDTH = 500;
+    @Getter
     private static final int TABLE_HEIGHT = 300;
     private static final int TABLE_X = 20;
+    @Getter
     private static final int TABLE_Y = 20;
     private static final int TABLE_CENTRE_X = TABLE_X + TABLE_WIDTH / 2;
+    private static final int PADDLE_X_OFFSET = 20;
     private static final int PADDLE_WIDTH = 10;
+    @Getter
     private static final int PADDLE_HEIGHT = 50;
-    private static final int BALL_SIZE = 10;
+
+    @Setter
+    @Getter
+    private int playerPaddleY  = TABLE_Y + TABLE_HEIGHT / 2 - PADDLE_HEIGHT / 2;
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        TODO: make this look nicer and bevelled
+//        TODO: make this look nicer and bevelled --> round rectangle
         g.drawRect(TABLE_X, TABLE_Y, TABLE_WIDTH, TABLE_HEIGHT);
         g.drawLine(TABLE_CENTRE_X, TABLE_Y, TABLE_CENTRE_X, TABLE_Y + TABLE_HEIGHT);
+        g.fillRect(TABLE_X + PADDLE_X_OFFSET, playerPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+        g.fillRect(TABLE_X + TABLE_WIDTH - PADDLE_X_OFFSET - PADDLE_WIDTH, TABLE_Y + TABLE_HEIGHT / 2 - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 
     @Override
@@ -29,12 +41,21 @@ public class Pong extends JPanel {
     }
 
     private static void createAndShowGui() {
-        Pong gameTable = new Pong();
+//        TODO: have initial menu screen --> set controls and colours, start game
         JFrame frame = new JFrame("DrawRect");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container contentPane = frame.getContentPane();
-        contentPane.add(gameTable);
+        Pong pong = new Pong();
+        MoveAction up = new MoveAction(pong, true);
+        MoveAction down = new MoveAction(pong, false);
+
+        pong.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0),
+                "movePlayerPaddleUp");
+        pong.getActionMap().put("movePlayerPaddleUp", up);
+        pong.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0),
+                "movePlayerPaddleDown");
+        pong.getActionMap().put("movePlayerPaddleDown", down);
+        frame.setContentPane(pong);
 
         frame.pack();
         frame.setLocationByPlatform(true);
@@ -42,6 +63,6 @@ public class Pong extends JPanel {
     }
 
     public static void main(String[] args) {
-        createAndShowGui();
+            SwingUtilities.invokeLater(Pong::createAndShowGui);
     }
 }
